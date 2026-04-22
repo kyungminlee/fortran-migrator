3a70d2f6d784bcb4aa0bde40a10aac1986a2a1ed
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2026-04-22 08:35:43 -0700

    Add external/MUMPS_5.8.2

---
+++ external/MUMPS_5.8.2/src/mumps_register_thread.c
@@ -0,0 +1,21 @@
+/*
+ *
+ *  This file is part of MUMPS 5.8.2, released
+ *  on Mon Jan 12 15:17:08 UTC 2026
+ *
+ *
+ *  Copyright 1991-2026 CERFACS, CNRS, ENS Lyon, INP Toulouse, Inria,
+ *  Mumps Technologies, University of Bordeaux.
+ *
+ *  This version of MUMPS is provided to you free of charge. It is
+ *  released under the CeCILL-C license 
+ *  (see doc/CeCILL-C_V1-en.txt, doc/CeCILL-C_V1-fr.txt, and
+ *  https://cecill.info/licences/Licence_CeCILL-C_V1-en.html)
+ *
+ */
+void mumps_register_thread_return()
+{
+  /*
+   * Registering tools will be available in the future.
+   */
+}
